4313950d8cc749427636d63671832cfc21cc9270
vkioresku <>
2021-03-20 21:56:25 +0200

    Setup project.

---
+++ src/components/Layout/index.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  width: 1400px;
+  margin: 0 auto;
+  padding: 0 30px;
+`;
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { Layout } from './Layout';
+export { GlobalStyles } from './GlobalStyles';
