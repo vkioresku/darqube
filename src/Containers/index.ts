bbc1272bbb2fddf031a7f03f6ef61235ad157b91
vkioresku <>
2021-03-21 21:12:26 +0200

    Add links.

---
+++ src/Containers/index.ts
@@ -0,0 +1,3 @@
+export { default as App } from './App';
+export { Bookmarks } from './Bookmarks';
+export { News } from './News';
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,4 +1,5 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+
 import newsReducer from '../containers/News/newsSlice';
 import paginationReducer from '../containers/News/paginationSlice';
 import bookmarksReducer from '../containers/Bookmarks/bookmarksSlice';
